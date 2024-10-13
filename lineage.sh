bc48990678e697d2bcad4604a5003a11bc866f1f
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-13 15:57:39 +0300

    Update lineage.sh

--- lineage.sh
+++ lineage.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests
 repo init --depth=1 --no-repo-verify -u https://github.com/Los-Ext/manifest.git -b lineage-21.0 --git-lfs -g default,-mips,-darwin,-notdefault
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b ext .repo/local_manifests
+git clone https://github.com/MohamedDevvv/Build- --depth 1 -b ext .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
@@ -17,11 +17,11 @@ git clone https://github.com/shravansayz/local_manifests --depth 1 -b ext .repo/
 rm -rf vendor/lineage-priv
 git clone https://github.com/shravansayz/private_keys.git -b rise vendor/lineage-priv
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=mohamed
 export BUILD_HOSTNAME=crave
 
 #build
 source build/envsetup.sh
-lunch lineage_RMX1901-user
+lunch lineage_gale-user
 m installclean
 m bacon
