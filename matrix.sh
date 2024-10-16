2bf4e728780d734b55519ed4b3bd5de8faa9302a
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-16 17:55:41 +0300

    Update matrix.sh

--- matrix.sh
+++ matrix.sh
@@ -7,24 +7,24 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b matrix .repo/local_manifests
+git clone https://github.com/MohamedDevvv/Build- --depth 1 -b main .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/android_frameworks_base_matrix.git -b 14.0 frameworks/base --depth=1
+git clone https://github.com/MohamedDevvv/android_frameworks_base_matrix.git -b 14.0 frameworks/base --depth=1
 
 #Private Keys
 rm -rf vendor/lineage-priv
-git clone https://github.com/shravansayz/private_keys.git -b mat vendor/lineage-priv
+git clone https://github.com/MohamedDevvv/private_keys.git -b mat vendor/lineage-priv
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=Mohamed
 export BUILD_HOSTNAME=crave
 
 #build
 source build/envsetup.sh
-lunch lineage_RMX1901-ap2a-user
+lunch lineage_gale-ap2a-user
 m installclean
 m bacon
