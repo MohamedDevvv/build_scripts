7cd18c36fd9cab6b6851aa3c6cd0fb2059f3e432
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-17 14:22:25 +0300

    Update droidx.sh

--- droidx.sh
+++ droidx.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests
 repo init --depth=1 -u https://github.com/DroidX-UI/manifest.git -b 14_v3 --git-lfs --depth=1
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b droidx .repo/local_manifests
+git clone https://github.com/MohamedDevvv/Build- --depth 1 -b main .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
@@ -19,11 +19,11 @@ git clone https://github.com/shravansayz/frameworks_base_droidx.git -b 14_v3 fra
 rm -rf vendor/droidx-priv
 git clone https://github.com/shravansayz/private_keys.git -b droidx vendor/droidx-priv --depth=1
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=Mohamed
 export BUILD_HOSTNAME=crave
 
 #build
 source build/envsetup.sh
-lunch droidx_RMX1901-ap2a-user
+lunch droidx_gale-ap2a-user
 make installclean
 mka bacon
