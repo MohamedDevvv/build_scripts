c1b2cb943e9328c0415914a6c909d72a811477a8
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-19 18:24:15 +0300

    Update and rename pixel.sh to Elixir.sh

---
+++ Elixir.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Removals
+rm -rf .repo/local_manifests
+
+# Initialize repo with specified manifest
+repo init --depth=1 -u https://github.com/Project-Elixir/manifest -b UNO --git-lfs
+
+# Clone local_manifests repository
+git clone https://github.com/MohamedDevvv/Build- --depth 1 -b main .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+#customs
+rm -rf frameworks/base
+git clone https://github.com/Project-Elixir/frameworks_base -b UNO frameworks/base --depth=1
+
+rm -rf vendor/aosp
+git clone https://github.com/Project-Elixir/vendor_aosp.git -b UNO vendor/aosp --depth=1
+
+rm -rf packages/apps/Settings 
+git clone https://github.com/Project-Elixir/packages_apps_Settings -b UNO-QPR2 packages/apps/Settings 
+
+export BUILD_USERNAME=Mohamed
+export BUILD_HOSTNAME=crave
+
+#build
+source build/envsetup.sh
+lunch aosp_gale-ap2a-userdebug
+make installclean
+mka bacon
