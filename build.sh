bfe5c0aee048e297dfd4623e869e07b6184fe045
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-20 07:53:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
+#SudoMohamed telegram
 # Removals
 rm -rf .repo/local_manifests
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/The-Pixel-Project/manifest -b fourteen-qpr3 -g default,-mips,-darwin,-notdefault --git-lfs
+repo init --depth=1 -u https://github.com/Project-Elixir/manifest -b UNO --git-lfs
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b tpp .repo/local_manifests
+git clone https://github.com/MohamedDevvv/Build- --depth 1 -b main .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_tpp.git -b fourteen-qpr3 frameworks/base --depth=1
+git clone https://github.com/Project-Elixir/frameworks_base.git -b UNO frameworks/base --depth=1
 
-rm -rf hardware/interfaces
-git clone https://github.com/shravansayz/hardware_interfaces.git -b fourteen-qpr3 hardware/interfaces --depth=1
+rm -rf vendor/aosp
+git clone https://github.com/Project-Elixir/vendor_aosp.git -b UNO vendor/aosp --depth=1
 
-rm -rf vendor/aosp/signing
-git clone https://github.com/shravansayz/private_keys.git -b pixelpro vendor/aosp/signing --depth=1
+rm -rf packages/apps/Settings 
+git clone https://github.com/Project-Elixir/packages_apps_Settings.git -b UNO-QPR2 packages/apps/Settings --depth=1
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=Mohamed
 export BUILD_HOSTNAME=crave
 
 #build
 source build/envsetup.sh
-lunch aosp_RMX1901-user
-make installclean
-make bacon
+lunch aosp_gale-user
+m installclean
+m bacon
