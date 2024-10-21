c25dce9fb6cc034e4ddde9fa6ba1273197db8de9
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-21 18:11:27 +0300

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -3,6 +3,7 @@
 rm -rf .repo/local_manifests && \
 repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b fourteen -git-lfs -g default,-mips,-darwin,-notdefault && \
 git clone https://github.com/MohamedDevvv/Build-.git -b main .repo/local_manifests && \
+/opt/crave/resync.sh
 export TARGET_BUILD_GAPPS=true ; \
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true ; \
 source build/envsetup.sh && \
