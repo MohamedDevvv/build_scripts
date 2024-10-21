ad25017993585e7cfa07925be218b48e924f81d1
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-21 06:56:37 +0300

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-# Removals
-rm -rf .repo/local_manifests
-
-# Initialize repo with specified manifest
-repo init -u https://github.com/yaap/manifest.git -b fourteen --git-lfs
-
-# Clone local_manifests repository
-git clone https://github.com/MohamedDevvv/Build-.git --depth 1 -b main .repo/local_manifests
-
-git clone https://github.com/yaap/frameworks_base/tree/fourteen.git --depth -b fourteen frameworks/base
-
-# Sync the repositories
-/opt/crave/resync.sh && /opt/crave/resync.sh
-
-export BUILD_USERNAME=shravan
-export BUILD_HOSTNAME=crave
-
-# Git-lfs
-repo forall -c 'git lfs install && git lfs pull'
-
-# Set up build environment
-source build/envsetup.sh
-lunch yaap_RMX1901-userdebug
-make installclean
+rm -rf .repo/local_manifests && \
+repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b fourteen -git-lfs -g default,-mips,-darwin,-notdefault && \
+git clone https://github.com/MohamedDevvv/Build-.git -b main .repo/local_manifests && \
+/opt/crave/resync.sh && \
+export TARGET_BUILD_GAPPS=true ; \
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true ; \
+source build/envsetup.sh && \
+lunch yaap_gale-user && \
+make installclean ; \
 m yaap
