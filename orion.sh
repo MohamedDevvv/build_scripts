8464a99153057d9839754df80e7040c3beaf61a3
MohamedDevvv <aaamohamed499@gmail.com>
2024-10-22 19:23:58 +0300

    Update orion.sh

--- orion.sh
+++ orion.sh
@@ -7,24 +7,24 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests --depth 1 -b orion .repo/local_manifests
+git clone https://github.com/MohamedDevvv/Build- --depth 1 -b main .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/android_frameworks_base_orion.git -b 14.0 frameworks/base --depth=1
+git clone https://github.com/OrionOS-prjkt/android_frameworks_base.git -b 14.0 frameworks/base --depth=1
 
 #Private Keys
 rm -rf vendor/lineage-priv
 git clone https://github.com/shravansayz/private_keys.git -b rise vendor/lineage-priv
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=Mohamed
 export BUILD_HOSTNAME=crave
-
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 #build
 source build/envsetup.sh
-lunch orion_RMX1901-ap2a-user
+lunch orion_gale-ap2a-user
 m installclean
 mka space
